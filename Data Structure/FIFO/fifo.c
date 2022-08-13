e5acaeaa142e122d4fab8bb59b3b2267302395c4
Amr Ahmed <amr.a.salhin@gmail.com>
2022-08-13 20:23:09 +0200

    FIFO Assignment

---
+++ Data Structure/FIFO/fifo.c
@@ -0,0 +1,84 @@
+/*
+ * fifo.c
+ *
+ *  Created on: Aug 13, 2022
+ *      Author: Amr Salhen
+ */
+
+#include "fifo.h"
+
+FIFO_Status init_FIFO(FIFO_BUF_t* fifo,data_type* buf){
+	//check if FIFO is NULL
+	if(!fifo->head || !fifo->base || !fifo->tail){
+		printf("========invalid FIFO=======");
+		return FIFO_NULL;
+	}
+	//FIFO initialization
+	fifo->length=width;
+	fifo->base=buf;
+	fifo->head=buf;
+	fifo->tail=buf;
+	fifo->count=0;
+	return FIFO_NO_ERROR;
+}
+FIFO_Status FIFO_enqueue(FIFO_BUF_t* fifo,data_type item){
+
+	if(!fifo->head || !fifo->base || !fifo->tail){
+		printf("========invalid FIFO=======");
+		return FIFO_NULL;
+	}
+	if(fifo->length==fifo->count){
+		printf("========FIFO is FULL=======");
+		return FIFO_FULL;
+	}
+	*fifo->head=item;
+	fifo->count++;
+	if(fifo->head==(fifo->base+(fifo->length*sizeof(data_type)))){
+		fifo->head=fifo->base;
+	}else{
+		(fifo->head)++;
+	}
+	return FIFO_NO_ERROR;
+}
+FIFO_Status FIFO_dequeue(FIFO_BUF_t* fifo,data_type* item){
+
+	if(!fifo->head || !fifo->base || !fifo->tail){
+		printf("========invalid FIFO=======");
+		return FIFO_NULL;
+	}
+	if(fifo->count==0){
+		printf("========FIFO is EMPTY=======");
+		return FIFO_EMPTY;
+	}
+
+	*item=*fifo->tail;
+	fifo->count--;
+	if(fifo->tail  == ((fifo->base)+(fifo->length)* sizeof(data_type))){
+		fifo->tail=fifo->base;
+	}else{
+		fifo->tail++;
+	}
+
+	return FIFO_NO_ERROR;
+}
+
+void FIFO_print(FIFO_BUF_t* fifo){
+	data_type* temp;
+	int i;
+	if (fifo->count == 0)
+		printf("\n====== FIFO is EMPTY ======\n");
+	else
+	{
+		temp = fifo->tail;
+		printf("\n======= FIFO print ======== \n");
+		for ( i = 0; i <fifo->count ; ++i) {
+			printf("\t %x \n",*temp);
+			if(temp  == ((fifo->base)+(fifo->length)* sizeof(data_type))){
+				temp=fifo->base;
+			}else{
+				temp++;
+			}
+		}
+		printf("========================\n");
+	}
+}
